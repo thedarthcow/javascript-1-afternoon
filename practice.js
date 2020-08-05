bad493ea03b3b728bceebda3a380007002e64046
Brad Carpenter <bradcarpenter@brads-air.lan>
2020-08-05 13:17:04 -0700

    finished

--- practice.js
+++ practice.js
@@ -2,41 +2,41 @@
 
 //Create a variable called myName that is a string data type
 
-let myName = 'Brad';
-
 //Code here
+
+let myName = 'Brad';
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-let myAge = 30;
-
 //Code here
+
+let myAge = 30;
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-let lovesCode = true;
-
 //Code here
+
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-let greatestFear = undefined; 
-
 //Code here
+
+let greatestFear = undefined; 
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-let devMountainGoal = null; 
-
 //Code here
+
+let devMountainGoal = null; 
 
 //////////////////PROBLEM 6////////////////////
 
@@ -45,32 +45,31 @@ let devMountainGoal = null;
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
+//Code here
+
 function greeting (name) {
   return "Hello, " + name; 
  } 
-
-//Code here
 
 //////////////////PROBLEM 7////////////////////
 
 //Write a function expression called newGreeting.
 //Give it the same functionality as the function greeting in Problem 6.
 
+//Code Here
+
 let newGreeting = function (name) {
   return "Hello, " + name; 
 }
-
-
-//Code Here
 
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-var groceries = ["apples", "milk", "eggs", "bread"];
-
 //Code Here
+
+var groceries = ["apples", "milk", "eggs", "bread"];
 
 //////////////////PROBLEM 9////////////////////
 
@@ -79,6 +78,8 @@ var groceries = ["apples", "milk", "eggs", "bread"];
 //name (a string), color (a string), age (a number),
 //and goodBoy (a boolean).
 
+//Code Here
+
 let dog = {
   name: "Matt",
   color: "Rainbow",
@@ -86,14 +87,13 @@ let dog = {
   goodBoy: true
 } 
 
-//Code Here
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-var devMountainClassPet = dog.name;
-
 //Code Here
+
+var devMountainClassPet = dog.name;
 
 //////////////////PROBLEM 10////////////////////
 
@@ -122,18 +122,18 @@ function nameCheck (name) {
 // that will be numbers.
 // The add function should return the two parameters added together
 
+//Code Here
+
 function add (num1, num2) {
   return num1 + num2;
 }
 
-//Code Here
-
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-let mathSUm = add(3,4); 
-
 //Code Here
+
+let mathSum = add(3,4); 
 
 //////////////////PROBLEM 12////////////////////
 
@@ -146,11 +146,11 @@ let mathSUm = add(3,4);
 
 // Code here
 
-function faveCorlorFinder(color) {
+function faveColorFinder(color) {
   if (color === "red") {
     return "red is a great color";
   } else if (color === "green") {
-   return 'green', return 'green is a solid favorite color'; 
+   return 'green is a solid favorite color'; 
   } else if (color === "black") {
     return "so trendy";
   } else {
@@ -195,21 +195,19 @@ let pondScope = ["duck", "realDuck"];
 
 //Create a variable called age with your age assigned to you
 
-let age = 30;
-
 // Code Here
+
+let age = 30;
 
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
-age = 31;
-
 // Code Here
+
+age = 31;
 
 // Good news! We can live forever. Set your age to 999
 
+// Code Here
+
 age = 999; 
-
-// Code here
-
-
